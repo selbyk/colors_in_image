19d21ed608c6a4d59a5fed47a887fe15fb491de5
selbyk <selby.kendrick@gmail.com>
2013-03-10 00:26:17 -0600

    Work on entropy by region

--- main.cpp
+++ main.cpp
@@ -18,12 +18,13 @@ using namespace boost::tuples;
 using namespace boost::numeric::ublas;
 
 //Configuration
-const string imageNames[8] = {"oxford.jpg", "panda.jpg","red_dress.jpg","girl_shirt.jpg","lotus.jpg","flower_dress.jpg","flower.jpg","earth.jpg"};
+const string imageNames[9] = {"oxford.jpg", "panda.jpg","red_dress.jpg","girl_shirt.jpg","lotus.jpg","flower_dress.jpg","flower.jpg","earth.jpg","amber_shirt.jpg"};
 #define RESIZE_TO 450
 #define THRESHOLD_MIN 2
 #define THRESHOLD_MAX 256
 #define NUM_VARIED_WEIGHTS 9
 #define PERCENT_TO_TEST 0.60  //Center % of image to check
+#define ENTROPY_THRESHOLD 70
 
 //Just defining some types
 typedef boost::tuple<int, int, int> rgb;
@@ -37,15 +38,22 @@ typedef struct{
 	unsigned int occurences;
 	unsigned int weight;
 	int varied_weight;
-	unsigned long int varied_weights[NUM_VARIED_WEIGHTS];
+	unsigned int entropy;
+	long int varied_weights[NUM_VARIED_WEIGHTS];
 	int avgX;
 	int avgY;
 } image_rgb;
 typedef struct{
+	list<int> entropies;
+	int max;
+	int min;
+} region_entropies;
+typedef struct{
 	rgb value;
 	unsigned int occurences;
 	unsigned int weight;
-	unsigned long int varied_weights[NUM_VARIED_WEIGHTS];
+	long int varied_weights[NUM_VARIED_WEIGHTS];
+	unsigned int entropy;
 	string hex;
 	string name;
 	int avgX;
@@ -56,6 +64,8 @@ typedef struct{
 void load_image(string filename);
 void kernel_1d_convolution(matrix<int> kernel);
 void kernel_2d_convolution(matrix<int> kernelX, matrix<int> kernelY, float divisor = 1);
+void calculate_entropies();
+void generate_entropy_squares();
 void extract_rgb_values();
 void sort_rgb_values();
 
@@ -68,6 +78,8 @@ bool image_color_compare (image_color first, image_color second); // comparison 
 //GLOBAL VARIABLES
 rgb8_image_t sourceImage;
 gray8_image_t convolutedImage;
+rgb8_image_t entropySquares;
+region_entropies entropies;
 list<image_rgb> rgbList;
 list<image_color> colorsInImage;
 list<color_def> colorDefinitions;
@@ -89,9 +101,12 @@ int main() {
 	
 	cout << "Colors loaded in " << t.elapsed() << "s\n";
 	
-	for( int i = 0; i < 8; ++i ){
+	for( int i = 0; i < 9; ++i ){
 		rgbList.clear();
 		colorsInImage.clear();
+		entropies.entropies.clear();
+		entropies.max = 0;
+		entropies.min = 999999;
 	  
 		//Load image
 		load_image("test_images/"+imageNames[i]);
@@ -123,7 +138,7 @@ int main() {
 		kernelY(0,1) = 0; kernelY(1,1) = 0; kernelY(2,1) = 0; 
 		kernelY(0,2) = 1; kernelY(1,2) = 2; kernelY(2,2) = 1; 
 		*/
-		/*
+		
 		//Scharr's edge-detect
 		kernelX(0,0) = 3; kernelX(1,0) = 0; kernelX(2,0) = -3; 
 		kernelX(0,1) = 10; kernelX(1,1) = 0; kernelX(2,1) = -10; 
@@ -132,18 +147,29 @@ int main() {
 		kernelY(0,0) = 3; kernelY(1,0) = 10; kernelY(2,0) = 3; 
 		kernelY(0,1) = 0; kernelY(1,1) = 0; kernelY(2,1) = 0; 
 		kernelY(0,2) = -3; kernelY(1,2) = -10; kernelY(2,2) = -3; 
-		*/
+		
 		//Prewitt edge-detect
-		kernelX(0,0) = -1; kernelX(1,0) = 0; kernelX(2,0) = 1; 
+		/*kernelX(0,0) = -1; kernelX(1,0) = 0; kernelX(2,0) = 1; 
 		kernelX(0,1) = -1; kernelX(1,1) = 0; kernelX(2,1) = 1; 
 		kernelX(0,2) = -1; kernelX(1,2) = 0; kernelX(2,2) = 1; 
 		
 		kernelY(0,0) = 1; kernelY(1,0) = 1; kernelY(2,0) = 1; 
 		kernelY(0,1) = 0; kernelY(1,1) = 0; kernelY(2,1) = 0; 
-		kernelY(0,2) = -1; kernelY(1,2) = -1; kernelY(2,2) = -1;
+		kernelY(0,2) = -1; kernelY(1,2) = -1; kernelY(2,2) = -1;*/
 		
-		kernel_2d_convolution( kernelX, kernelY ); //Creates a greyscale edge detect convolution
+		kernel_2d_convolution( kernelX, kernelY, 11 ); //Creates a greyscale edge detect convolution
 
+		calculate_entropies();
+		//Test print
+		for( list<int>::iterator it = entropies.entropies.begin(); it != entropies.entropies.end(); ++it ){
+			//rgb2 = get<0>(*it);
+			cout << "R: " << *it << endl;
+		}
+		cout << "Min: " << entropies.min << endl;
+		cout << "Max: " << entropies.max << endl;
+		
+		//generate_entropy_squares();
+		
 		extract_rgb_values(); //Extracts and weights each pixel
 
 		//Test print
@@ -175,11 +201,13 @@ int main() {
 		}*/
 		jpeg_write_view(string("images/input")+(char)(i+49)+".jpg", view(sourceImage) );
 		jpeg_write_view(string("images/input_convoluted")+(char)(i+49)+".jpg", view(convolutedImage) );
+		jpeg_write_view(string("images/input_ent_sqs")+(char)(i+49)+".jpg", view(entropySquares) );
 		
 		myfile << "<div class = 'item'>\n";
 		myfile << "<div class = 'image'>\n" <<
 			  "<img src='images/input" << i+1 << ".jpg'>\n" <<
-			  "<img src='images/input_convoluted" << i+1 << ".jpg'>\n</div>\n";  
+			  "<img src='images/input_convoluted" << i+1 << ".jpg'>\n" <<
+			  "<img src='images/input_ent_sqs" << i+1 << ".jpg'>\n</div>\n";
 		for( list<image_color>::iterator it = colorsInImage.begin(); it != colorsInImage.end(); ++it ){
 			myfile << "<div class = 'color' style='border:solid 1px " << it->hex << ";'>\n" <<
 				  "<div class = 'block' style='background-color:" << it->hex << ";'></div>\n" <<
@@ -189,11 +217,12 @@ int main() {
 				", " << get<2>(it->value) <<
 				") * Hex: " << it->hex <<
 				" * Occurences: " << it->occurences << 
-				" </br> Weight: " << it->weight <<
-				"  * Varied Weights: (" << it->varied_weights[0];
+				" * Average Position: (" << it->avgX << ", " << it->avgY << ")\n" <<
+				//" </br> Weight: " << it->weight <<
+				"</br># Neighboring Pixels Over Treshold: (" << it->varied_weights[0];
 			for( int i = 1; i < NUM_VARIED_WEIGHTS; ++i )
 				myfile << ", " << it->varied_weights[i];
-			myfile << ") * Average Position: (" << it->avgX << ", " << it->avgY << ")\n</div>\n";
+			myfile << ") Total Entropy: " << it->entropy << "\n</div>\n";
 		}
 		myfile << "</div>\n";
 	}
@@ -289,7 +318,7 @@ void kernel_2d_convolution(matrix<int> kernelX, matrix<int> kernelY, float divis
 			*dstLoc = sqrt( pow(dstXValue, 2) + pow( dstYValue, 2) )/divisor;
 			
 			//Get rid of horizontal and vertical edges
-			float degrees;
+			/*float degrees;
 			dstXValue != 0 ? degrees = atan ((float)dstYValue/dstXValue)*57.2957795 : degrees = 0;
 			if( degrees < 0 )
 				degrees += 180;
@@ -298,7 +327,7 @@ void kernel_2d_convolution(matrix<int> kernelX, matrix<int> kernelY, float divis
 			else if( degrees > 67.5 && degrees < 112.5 )
 				degrees = 90;
 			if( degrees == 0 || degrees == 90 )
-				*dstLoc = 0;
+				*dstLoc = 0;*/
 			//cout << atan ((float)dstYValue/dstXValue)*57.2957795 << endl;
 
 			++srcLoc.x();
@@ -308,6 +337,258 @@ void kernel_2d_convolution(matrix<int> kernelX, matrix<int> kernelY, float divis
 		dstLoc.x() -= xMax;
 		srcLoc.y()++;
 		dstLoc.y()++;
+	}
+}
+
+void calculate_entropies(){
+	gray8_view_t test = view(convolutedImage);
+
+	//Perform some commonly used operations so they aren't done too often
+	int imageWidth = sourceImage.width(), imageHeight = sourceImage.height();
+	
+	int xSteps = imageWidth/10;
+	int xRemainder = imageWidth%10;
+	int ySteps = imageHeight/10;
+	int yRemainder = imageHeight%10;
+	
+	float xMultiplier, yMultiplier;
+	if( xRemainder != 0 )
+		 xMultiplier = 10/xRemainder;
+	if( yRemainder != 0 )
+		yMultiplier = 10/yRemainder;
+	
+	int entropy;
+
+	//Prepare to traverse pixels!
+	int x,y, o, a, i, j;
+	rgb8_pixel_t pixel;
+	gray8_view_t::xy_locator testLoc;
+
+	entropies.max = 0;
+	entropies.min = 9999999;
+	for( y = 0; y < ySteps; y++ ){
+		for( x = 0; x < xSteps; x++ ){
+			testLoc = test.xy_at(x*10+1,y*10+1);
+
+			entropy = 0;
+			for( o = 0; o < 10; ++o ){
+				for( a = 0; a <= 10; ++a ){
+					for( i = -1; i <= 1; ++i )
+						for( j = -1; j <= 1; ++j )
+							entropy += abs( *testLoc - (testLoc(i, j)) );
+					testLoc.x()++;
+				}
+				testLoc.x() -= 10;
+				testLoc.y()++;
+			}
+			entropies.entropies.push_back( entropy );
+			if( entropy > entropies.max )
+				entropies.max = entropy;
+			else if( entropy < entropies.min )
+				entropies.min = entropy;
+			
+		}
+		if( xRemainder != 0 ){
+			testLoc = test.xy_at(xSteps*10+1,ySteps*10+1);
+			entropy = 0;
+			for( o = 0; o < 10; ++o ){
+				for( a = 0; a < xRemainder; ++a ){
+					for( i = -1; i <= 1; ++i )
+						for( j = -1; j <= 1; ++j )
+							entropy += abs( *testLoc - (testLoc(i, j)) );
+					testLoc.x()++;
+				}
+				testLoc.x() -= 10;
+				testLoc.y()++;
+			}
+			entropy *= xMultiplier;
+			entropies.entropies.push_back( entropy );
+			if( entropy > entropies.max )
+				entropies.max = entropy;
+			else if( entropy < entropies.min )
+				entropies.min = entropy;
+		}
+	}
+	if( yRemainder != 0 )
+		for( x = 0; x < xSteps; x++ ){
+			testLoc = test.xy_at(x*10+1,ySteps*10+1);
+
+			entropy = 0;
+			for( o = 0; o < yRemainder; ++o ){
+				for( a = 0; a <= 10; ++a ){
+					for( i = -1; i <= 1; ++i )
+						for( j = -1; j <= 1; ++j )
+							entropy += abs( *testLoc - (testLoc(i, j)) );
+					testLoc.x()++;
+				}
+				testLoc.x() -= 10;
+				testLoc.y()++;
+			}
+			entropy *= yMultiplier;
+			entropies.entropies.push_back( entropy );
+			if( entropy > entropies.max )
+				entropies.max = entropy;
+			else if( entropy < entropies.min )
+				entropies.min = entropy;
+		}
+	if( xRemainder != 0 && yRemainder != 0 ){
+		testLoc = test.xy_at(x*10+1,ySteps*10+1);
+
+		entropy = 0;
+		for( o = 0; o < yRemainder; ++o ){
+			for( a = 0; a <= xRemainder; ++a ){
+				for( i = -1; i <= 1; ++i )
+					for( j = -1; j <= 1; ++j )
+						entropy += abs( *testLoc - (testLoc(i, j)) );
+				testLoc.x()++;
+			}
+			testLoc.x() -= 10;
+			testLoc.y()++;
+		}
+		entropy *= yMultiplier *= xMultiplier;
+		entropies.entropies.push_back( entropy );
+		if( entropy > entropies.max )
+			entropies.max = entropy;
+		else if( entropy < entropies.min )
+			entropies.min = entropy;
+	}
+}
+
+void generate_entropy_squares(){
+	entropySquares.recreate( sourceImage.dimensions() );
+	//Create grayscale copy of original image
+	copy_pixels( const_view(sourceImage), view(entropySquares) );
+	
+	rgb8_view_t test = view(entropySquares);  
+
+	//Perform some commonly used operations so they aren't done too often
+	int imageWidth = sourceImage.width(), imageHeight = sourceImage.height();
+	
+	int xSteps = imageWidth/10;
+	int xRemainder = imageWidth%10;
+	int ySteps = imageHeight/10;
+	int yRemainder = imageHeight%10;
+	
+	float xMultiplier, yMultiplier;
+	if( xRemainder != 0 )
+		 xMultiplier = 10/xRemainder;
+	else
+		xMultiplier = 0;
+	if( yRemainder != 0 )
+		yMultiplier = 10/yRemainder;
+	else
+		yMultiplier = 0;
+	
+	float entropyMultiplier = 510/entropies.max;
+	
+	int offset = ((float)entropies.max*entropyMultiplier+(float)entropies.min*entropyMultiplier)/2;
+	int entropy;
+	list<int>::iterator it = entropies.entropies.begin();
+	//it != entropies.entropies.end();
+	//Prepare to traverse pixels!
+	int x,y, o, a;
+	rgb8_view_t::xy_locator testLoc;
+	
+	rgb8s_pixel_t pixelColor;
+
+	entropies.max = 0;
+	entropies.min = 99999;
+	for( y = 0; y < ySteps; y++ ){
+		for( x = 0; x < xSteps; x++ ){
+			testLoc = test.xy_at(x*10+1,y*10+1);
+			
+			entropy = (*it)*entropyMultiplier-offset;
+			if( entropy < 0 ){
+				at_c<0>(pixelColor) = abs( entropy );
+				at_c<1>(pixelColor) = 0;
+				at_c<2>(pixelColor) = 0;
+			} else {
+				at_c<0>(pixelColor) = entropy;
+				at_c<1>(pixelColor) = entropy;
+				at_c<2>(pixelColor) = entropy;
+			}
+			
+			for( o = 0; o < 10; ++o ){
+				for( a = 0; a < 10; ++a ){
+					at_c<0>(*testLoc) = at_c<0>(pixelColor);
+					at_c<1>(*testLoc) = at_c<1>(pixelColor);
+					//at_c<2>(*testLoc) = at_c<2>(pixelColor);
+					testLoc.x()++;
+				}
+				testLoc.x() -= 10;
+				testLoc.y()++;
+			}
+			it++;
+		}
+		if( xRemainder != 0 ){
+			testLoc = test.xy_at(xSteps*10+1,ySteps*10+1);
+			entropy = (*it)*entropyMultiplier-offset;
+			/*if( entropy < 0 ){
+				at_c<0>(pixelColor) = abs( entropy );
+				at_c<1>(pixelColor) = 0;
+				at_c<2>(pixelColor) = 0;
+			} else {
+				at_c<0>(pixelColor) = entropy;
+				at_c<1>(pixelColor) = entropy;
+				at_c<2>(pixelColor) = entropy;
+			}
+			for( o = 0; o < 10; ++o ){
+				for( a = 0; a < xRemainder; ++a ){
+					*testLoc = pixelColor;
+					testLoc.x()++;
+				}
+				testLoc.x() -= 10;
+				testLoc.y()++;
+			}*/
+			it++;
+		}
+	}
+	if( yRemainder != 0 )
+		for( x = 0; x < xSteps; x++ ){
+			testLoc = test.xy_at(x*10+1,ySteps*10+1);
+
+			entropy = (*it)*entropyMultiplier-offset;
+			/*if( entropy < 0 ){
+				at_c<0>(pixelColor) = abs( entropy );
+				at_c<1>(pixelColor) = 0;
+				at_c<2>(pixelColor) = 0;
+			} else {
+				at_c<0>(pixelColor) = entropy;
+				at_c<1>(pixelColor) = entropy;
+				at_c<2>(pixelColor) = entropy;
+			}
+			for( o = 0; o < yRemainder; ++o ){
+				for( a = 0; a <= 10; ++a ){
+					*testLoc = pixelColor;
+					testLoc.x()++;
+				}
+				testLoc.x() -= 10;
+				testLoc.y()++;
+			}*/
+			it++;
+		}
+	if( xRemainder != 0 && yRemainder != 0 ){
+		testLoc = test.xy_at(x*10+1,ySteps*10+1);
+
+		entropy = (*it)*entropyMultiplier-offset;
+		/*if( entropy < 0 ){
+			at_c<0>(pixelColor) = abs( entropy );
+			at_c<1>(pixelColor) = 0;
+			at_c<2>(pixelColor) = 0;
+		} else {
+			at_c<0>(pixelColor) = entropy;
+			at_c<1>(pixelColor) = entropy;
+			at_c<2>(pixelColor) = entropy;
+		}
+		for( o = 0; o < yRemainder; ++o ){
+			for( a = 0; a <= xRemainder; ++a ){
+				*testLoc = pixelColor;
+				testLoc.x()++;
+			}
+			testLoc.x() -= 10;
+			testLoc.y()++;
+		}*/
+		it++;
 	}
 }
 
@@ -334,10 +615,15 @@ void extract_rgb_values(){
 				currentRgb.varied_weights[i] = 0;
 			
 			currentRgb.varied_weight = 0;
+			currentRgb.entropy = 0;
 			for( i = -1; i <= 1; ++i )
-				for( j = -1; j <= 1; ++j )
+				for( j = -1; j <= 1; ++j ){
 					if( abs( *testLoc - (testLoc(i, j)) ) >= THRESHOLD_MIN && abs( *testLoc - (testLoc(i, j)) ) <= THRESHOLD_MAX )
 						currentRgb.varied_weight++;
+					currentRgb.entropy += abs( *testLoc - (testLoc(i, j)) );
+				}
+			if( currentRgb.entropy < ENTROPY_THRESHOLD )
+				currentRgb.entropy = 0;
 			currentRgb.varied_weights[currentRgb.varied_weight]++;
 
 			pixel = *srcLoc;
@@ -421,6 +707,7 @@ void sort_rgb_values(){
 		color.weight = it->weight;
 		color.avgX = it->avgX;
 		color.avgY = it->avgY;
+		color.entropy = it->entropy;
 		for( int i = 0; i < NUM_VARIED_WEIGHTS; ++i )
 			color.varied_weights[i] = it->varied_weights[i];
 		insert_image_color( color );
@@ -439,6 +726,7 @@ void insert_image_rgb(image_rgb rgb){
 		}else{
 			rgb.avgX = (rgb.avgX*rgb.occurences+it->avgX*it->occurences)/(rgb.occurences+it->occurences);
 			rgb.avgY = (rgb.avgY*rgb.occurences+it->avgY*it->occurences)/(rgb.occurences+it->occurences);
+			rgb.entropy += it->entropy;
 			rgb.occurences += it->occurences;
 			rgb.weight += it->weight;
 			for( int i = 0; i < NUM_VARIED_WEIGHTS; ++i )
@@ -463,6 +751,7 @@ void insert_image_color(image_color color){
 			color.avgY = (color.avgY*color.occurences+it->avgY*it->occurences)/(color.occurences+it->occurences);
 			color.occurences += it->occurences;
 			color.weight += it->weight;
+			color.entropy += it->entropy;
 			for( int i = 0; i < NUM_VARIED_WEIGHTS; ++i )
 				color.varied_weights[i] += it->varied_weights[i];
 			it = colorsInImage.erase(it);
@@ -472,12 +761,13 @@ void insert_image_color(image_color color){
 }
 
 bool image_color_compare (image_color first, image_color second){
-	unsigned long long int firstWeight = 0, secondWeight = 0;
+	long long int firstWeight = 0, secondWeight = 0;
 	for( int i = 0; i < NUM_VARIED_WEIGHTS; ++i ){
 		firstWeight += first.varied_weights[i]*i;
 		secondWeight += second.varied_weights[i]*i;
 	}
 	
-	if( firstWeight > secondWeight ) return true;
+	//if( firstWeight > secondWeight ) return true;
+	if( (first.entropy+firstWeight)/2 > (second.entropy+secondWeight)/2 ) return true;
 	else return false;
 }
